c732716de3ecf30874af80b2b62c0e9e9ccd1f13
Matt Berg <mberg@buildafrica.org>
2013-11-16 12:19:21 +0200

    MB: Burkina Data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -55,24 +55,75 @@ pecsApp.controller('burkinaCtrl', function ($scope, $http) {
 
     $scope.survey = {};
     $scope.country = "Burkina Faso";
-
     // get data from bamboo
     $http({
         method: 'GET',
-        url: ''
+        url: 'http://bamboo.io/datasets/051b9b737d7b4622b2542667bfe416fc'
     })
         .success(function (data, status, headers, config) {
             // See here, we are now assigning this username
             // to our existing model!
             $scope.survey = data;
-
-
         })
         .error(function (data, status, headers, config) {
             // something went wrong :(
         });
 
+    $http({
+        method: 'GET',
+        url: 'http://bamboo.io/datasets/bc6e118e59e141cc96163a0394bcf0c4'
+    })
+        .success(function (data, status, headers, config) {
+            // See here, we are now assigning this username
+            // to our existing model!
+            $scope.area = data;
+        })
+        .error(function (data, status, headers, config) {
+            // something went wrong :(
+        });
+
+    $http({
+        method: 'GET',
+        url: 'http://bamboo.io/datasets/3b1a363b9f0846e3bbd7f6cc09218a96'
+    })
+        .success(function (data, status, headers, config) {
+            // See here, we are now assigning this username
+            // to our existing model!
+            $scope.summary = data;
+        })
+        .error(function (data, status, headers, config) {
+            // something went wrong :(
+        });
+
+    $http({
+        method: 'GET',
+        url: 'http://bamboo.io/datasets/0042a1b4d6bb4c708ab2b770ae4384b6'
+    })
+        .success(function (data, status, headers, config) {
+            // See here, we are now assigning this username
+            // to our existing model!
+            $scope.survey_dates = data;
+        })
+        .error(function (data, status, headers, config) {
+            // something went wrong :(
+        });
+
+    $http({
+        method: 'GET',
+        url: 'http://bamboo.io/datasets/ad848bd3f6ab4ca69a7a4171d87543b9'
+    })
+        .success(function (data, status, headers, config) {
+            // See here, we are now assigning this username
+            // to our existing model!
+            $scope.equipes = data;
+        })
+        .error(function (data, status, headers, config) {
+            // something went wrong :(
+        });
+
+
 });
+
 
 pecsApp.controller('guineaCtrl', function ($scope, $http) {
 
