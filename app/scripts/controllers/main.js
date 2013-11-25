005a999f0290a477ac626671b7c1416cf59de0ce
Matt Berg <mberg@buildafrica.org>
2013-11-25 13:19:38 +0300

    Updated Burkina page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -53,97 +53,22 @@
 
     });
 
-
-    pecsApp.controller('burkinaCtrl', function ($scope, $http) {
-
-        $scope.survey = {};
-        $scope.country = "Burkina Faso";
-        // get data from bamboo
-        $http({
-            method: 'GET',
-            url: 'http://bamboo.io/datasets/051b9b737d7b4622b2542667bfe416fc'
-        })
-            .success(function (data, status, headers, config) {
-                // See here, we are now assigning this username
-                // to our existing model!
-                $scope.survey = data;
-            })
-            .error(function (data, status, headers, config) {
-                // something went wrong :(
-            });
-
-        $http({
-            method: 'GET',
-            url: 'http://bamboo.io/datasets/bc6e118e59e141cc96163a0394bcf0c4'
-        })
-            .success(function (data, status, headers, config) {
-                // See here, we are now assigning this username
-                // to our existing model!
-                $scope.area = data;
-            })
-            .error(function (data, status, headers, config) {
-                // something went wrong :(
-            });
-
-        $http({
-            method: 'GET',
-            url: 'http://bamboo.io/datasets/3b1a363b9f0846e3bbd7f6cc09218a96'
-        })
-            .success(function (data, status, headers, config) {
-                // See here, we are now assigning this username
-                // to our existing model!
-                $scope.summary = data;
-            })
-            .error(function (data, status, headers, config) {
-                // something went wrong :(
-            });
-
-        $http({
-            method: 'GET',
-            url: 'http://bamboo.io/datasets/0042a1b4d6bb4c708ab2b770ae4384b6'
-        })
-            .success(function (data, status, headers, config) {
-                // See here, we are now assigning this username
-                // to our existing model!
-                $scope.survey_dates = data;
-            })
-            .error(function (data, status, headers, config) {
-                // something went wrong :(
-            });
-
-        $http({
-            method: 'GET',
-            url: 'http://bamboo.io/datasets/ad848bd3f6ab4ca69a7a4171d87543b9'
-        })
-            .success(function (data, status, headers, config) {
-                // See here, we are now assigning this username
-                // to our existing model!
-                $scope.equipes = data;
-            })
-            .error(function (data, status, headers, config) {
-                // something went wrong :(
-            });
-
-
-    });
-
-
-    pecsApp.controller('guineaCtrl', [ "$scope", "$http", "OnadataService", function ($scope, $http, ona) {
+    pecsApp.controller('burkinaCtrl', [ "$scope", "$http", "OnadataService", function ($scope, $http, ona) {
 
         // enable CORS
         $http.defaults.useXDomain = true;
         delete $http.defaults.headers.common['X-Requested-With'];
         // onadata api token
-        $http.defaults.headers.common.Authorization = 'Token b632f0b82d0a888f03f3c747dc8db7b79af9f581';
+        $http.defaults.headers.common.Authorization = 'Token ca87b3231663ca9823a763d19866dfaf0fec5dcc';
 
         $scope.survey = {};
-        $scope.country = "Guinea";
+        $scope.country = "Burkina Faso";
         // get form info from ona.io
         var query = {
             group: 'enqueteur',
-            user: 'ukanga',
-            formid: 'Meres_gardiennes_15-11-13',
-            site: 'ukw530:8000'
+            user: 'hkiburkinadata',
+            formid: 'mere_enq',
+            site: 'ona.io'
         };
         query.name = 'enqueteur';
         $scope.survey = ona.query(query);
@@ -166,4 +91,57 @@
         };
 
     }]);
+
+
+    pecsApp.controller('guineaCtrl', [ "$scope", "$http", "OnadataService", function ($scope, $http, ona) {
+
+        // enable CORS
+        $http.defaults.useXDomain = true;
+        delete $http.defaults.headers.common['X-Requested-With'];
+        // onadata api token
+        $http.defaults.headers.common.Authorization = 'Token e056d923e8a8a5ccb2fd64cd04e9bcefedc27ab0';
+
+        $scope.survey = {};
+        $scope.country = "Guinea";
+        // get form info from ona.io
+        var query = {
+            group: 'enqueteur',
+            user: 'hkiguineadata',
+            formid: 'Meres_gardiennes_15-11-13',
+            site: 'ona.io'
+        };
+        query.name = 'enqueteur';
+        $scope.survey = ona.query(query);
+        $scope.enqueteurGrid = {data: 'survey',
+            columnDefs: [{field:'enqueteur', displayName:'Enqueteur'}, {field:'count', displayName:'# Surveys'}]
+        };
+
+        query.group = '_submission_time';
+        query.name = 'day';
+        $scope.byday = ona.query(query);
+        $scope.bydayGrid = {data: 'byday',
+            columnDefs: [{field:'day', displayName:'Date'}, {field:'count', displayName:'# Surveys'}]
+        };
+
+        query.group = 'village';
+        query.name = 'village';
+        $scope.area = ona.query(query);
+        $scope.villageGrid = {data: 'area',
+            columnDefs: [{field:'village', displayName:'Village'}, {field:'count', displayName:'# Surveys'}]
+        };
+
+        query.group = 'milieu_sous-prefecture';
+        query.name = 'milieu_sp';
+        $scope.sp = ona.query(query);
+        $scope.spGrid = {data: 'sp',
+            columnDefs: [{field:'milieu_sp', displayName:'Sous-Prefecture'}, {field:'count', displayName:'# Surveys'}]
+        };
+
+
+    }]);
+
+
+
+
+
 })();
--- app/scripts/services/onadataService.js
+++ app/scripts/services/onadataService.js
@@ -5,7 +5,7 @@
     angular.module('hkipecsApp')
       .service('OnadataService', ["$resource", function Onadataservice($resource) {
           return $resource(
-              'http://:site/api/v1/stats/submissions/:user/:formid',
+              'https://:site/api/v1/stats/submissions/:user/:formid',
               {user: 'demouser', formid: 'formid', site: 'ona.io'},
               {query: {method: 'GET', params: {group: 'enqueteur'}, withCredentials: true, isArray: true}}
           );
